756a114491a479c7780933531383a58dcb86ac99
prorocky <oshadian@ucsc.edu>
2022-09-19 21:53:24 -0700

    will this work?

--- Assets/circleTest.cs
+++ Assets/circleTest.cs
@@ -23,7 +23,6 @@ public class circleTest : MonoBehaviour
     void FixedUpdate()
     {
         returnColliders = Physics2D.OverlapCircleAll(transform.position, 6f, layer);
-        print(layer);
     }
 
     void OnDrawGizmos() {
