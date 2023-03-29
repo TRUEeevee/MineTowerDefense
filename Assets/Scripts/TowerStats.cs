f6518f19842077180dfcc1fd45442aa73a5f5f41
Nate <Nathangrzywacz333@gmail.com>
2023-03-29 17:13:12 -0400

    restructure of TowerStats and addition of comments for future plans

--- Assets/Scripts/TowerStats.cs
+++ Assets/Scripts/TowerStats.cs
@@ -3,111 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public enum TowerType{
-    Archer,
-    Melee,
-    Grief
-}
-
-// We need to completely overhaul or replace this class me thinks.
 public class TowerStats : MonoBehaviour
 {
-    private int _price;
-    private int _range;
-    private float _attackSpeed;
-    private int _attackDamage;
-    private int _pierce;
+    // this script will be attached to each towers prefab and be set there.
+
+    //each tower afterwards will have access to both reference and change its own stats, based on its upgrades, buffs, and enchants freely.
+
+    [SerializeField]
+    private int _range, _attackDamage, _pierce, _value;
+    [SerializeField]
+    private float _attackSpeed ;
+    [SerializeField]
     private (int, int) _upgradePath; //tuple containing the state of the selected towers upgrades
+    [SerializeField]
+    private string towerName;
 
-    public int price { get {return _price; } private set { _price = value; } }
-    public int range { get {return _range; } private set { _range = value; } }
-    public float attackSpeed { get {return _attackSpeed; } private set { _attackSpeed = value; } }
-    public int attackDamage { get {return _attackDamage; } private set { _attackDamage = value; } }
-    public int pierce { get {return _pierce; } private set { _pierce = value; } }
-    public (int,int) upgradePath { get { return _upgradePath; } private set { _upgradePath = value; } }
-
-    public TowerType TowerType { get; internal set; }
-
-    // extra misc stat? Archer: Projectile Speed -+-+-+- Melee: -+-+-+- Griefer: 
-
-    // default constructor (should never be called)
-    public TowerStats(): this(null) {}
-
-    // constructor using tower type
-    public TowerStats(TowerType towerType) {          
-        switch (towerType) {
-            case TowerType.Archer:
-                _price = 150;
-                _range = 4;
-                _attackSpeed = 3;
-                _attackDamage = 50;
-                _pierce = 1;
-                _upgradePath = (0, 0);
-                break;
-            case TowerType.Melee:
-                _price = 100;
-                _range = 2;
-                _attackSpeed = 4;
-                _attackDamage = 55;
-                _pierce = 0;
-                _upgradePath = (0, 0);
-                break;
-            case TowerType.Grief:
-                _price = 175;
-                _range = 4;
-                _attackSpeed = 3;
-                _attackDamage = 50;
-                _pierce = 0;
-                _upgradePath = (0, 0);
-                break;
-            default:
-                _price = 0;
-                _range = 0;
-                _attackSpeed = 0;
-                _attackDamage = 0;
-                _pierce = 0;
-                _upgradePath = (0, 0);
-                break;
-        }
+    public void IncreaseRange(int amount)//takes an amount and adds to the towers range
+    {
+        _range += amount;
     }
-
-    // constructor using stats, mainly used for upgrade modules
-    public TowerStats(int price, int range, float attackSpeed, int attackDamage, int pierce) {
-        _price = price;
-        _range = range;
-        _attackSpeed = attackSpeed;
-        _attackDamage = attackDamage;
-        _pierce = pierce;
+    public void IncreaseAttackSpeed(int amount)//takes an amount and adds to the towers attack speed
+    {
+        _attackSpeed += amount;
     }
-
-    // copy constructor
-    public TowerStats(TowerStats stats) {
-        _price = stats._price;
-        _range = stats._range;
-        _attackSpeed = stats._attackSpeed;
-        _attackDamage = stats._attackDamage;
-        _pierce = stats._pierce;
+    public void IncreaseAttackDamage(int amount)//takes an amount and adds to the towers damage
+    {
+        _attackDamage += amount;
     }
-
-    public static TowerStats operator + (TowerStats curStats, TowerStats upStats) {
-        TowerStats newStats = new TowerStats(curStats);
-        newStats.updateStats(upStats);
-        return newStats;
+    public void IncreasePierce(int amount) //takes an amount and adds to the towers pierce
+    {
+        _pierce += amount;
     }
-
-    // given stats, update tower stats to add stats
-    private void updateStats(TowerStats towerStats) {
-        _range += towerStats._range;
-        _attackSpeed += towerStats._attackSpeed;
-        _attackDamage += towerStats._attackDamage;
-        _pierce += towerStats._pierce;
+    public void UpdateUpgradePath((int, int) newPath) //takes a tuple and assigns that as the current upgrade path.
+    {
+        _upgradePath = newPath;
     }
-
-    // update tower stats to add specific bonuses
-    public void updateStats(int price, int range, float attackSpeed, int attackDamage, int pierce) {
-        _range += range;
-        _attackSpeed += attackSpeed;
-        _attackDamage += attackDamage;
-        _pierce += pierce;
+    public void UpdateValue(int amount) //takes an amount and adds to the towers value
+    {
+        _value += amount;
     }
 }
